785f02df2da616a49d8ce80f8f054ebce34ac577
HotWhiskey <ekaterinakozlova90@gmail.com>
2020-08-01 20:19:46 +0300

    Add credit card

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -39,7 +39,8 @@ export class CheckoutComponent implements OnInit {
         nameOnCard: [''],
         cardNumber: [''],
         securityCode: [''],
-        expirationMonth: ['']
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
   }
